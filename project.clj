2d5ec8c223c389ec3f53d3d2981efc7f4bba2b1b
Sean Bowman <pico303@gmail.com>
2011-10-13 15:58:45 -0600

    Updated to Clojure 1.3.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject org.clojars.sbowman/clj-apache-http "2.3.1"
- :description "Clojure HTTP library using the Apache HttpClient.  Fix bug with escaped characters in URLs."
+(defproject org.clojars.sbowman/clj-apache-http "2.4.0"
+ :description "Clojure HTTP library using the Apache HttpClient.  Fix bug with escaped characters in URLs, and updated to Clojure 1.3 and HTTP 4.1.2/4.1.3."
  :dev-dependencies [[lein-clojars "0.7.0"]]
- :dependencies [[org.clojure/clojure "1.2.1"]
-                [org.clojure/clojure-contrib "1.2.0"]
-                [org.apache.httpcomponents/httpcore "4.0.1"]
-                [org.apache.httpcomponents/httpmime "4.0.1"]
+ :dependencies [[org.clojure/clojure "1.3.0"]
+                [org.clojure/data.json "0.1.1"]
+                [org.apache.httpcomponents/httpcore "4.1.3"]
+                [org.apache.httpcomponents/httpmime "4.1.2"]
                 [commons-logging/commons-logging "1.1.1"]
-                [org.apache.httpcomponents/httpclient "4.0.1"]])
+                [org.apache.httpcomponents/httpclient "4.1.2"]])
